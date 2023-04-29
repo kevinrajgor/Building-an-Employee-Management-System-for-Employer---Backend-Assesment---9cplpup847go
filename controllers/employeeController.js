2fde65cc6ffa5c404d7d285877b54e8010c11a0d
Kevin Rajgor <kevinrajg95@gmail.com>
2023-04-29 19:40:48 +0530

    Updated

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -4,6 +4,9 @@ const Employee = require('../models/employeeModel');
 const createEmployee = async (req, res) => {
   try {
     // Write a code here to store Employee data
+    const employee = new Employee(req.body);
+    const newEmployee = await employee.save();
+    res.status(201).json({ newEmployee });
   } catch (err) {
     res.status(500).json({ error: 'Failed to create employee' });
   }
@@ -13,6 +16,11 @@ const createEmployee = async (req, res) => {
 const getEmployee = async (req, res) => {
   try {
     // Write a code here to get Employee from a Particular id
+    const employee = await Employee.findById(req.params.id);
+    if (!employee) {
+      return res.status(404).json({ error: "Employee not found" });
+    }
+    res.status(200).json(employee);
   } catch (err) {
     res.status(500).json({ error: 'Failed to get employee details' });
   }
@@ -22,6 +30,13 @@ const getEmployee = async (req, res) => {
 const updateEmployee = async (req, res) => {
   try {
     //Write a code here for updating Employee details using 'PUT' request
+    const employee = await Employee.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!employee) {
+      return res.status(404).json({ error: "Employee not found" });
+    }
+    res.status(200).json({ message: "Employee details updated successfully" });
   } catch (err) {
     res.status(500).json({ error: 'Failed to update employee details' });
   }
@@ -30,6 +45,11 @@ const updateEmployee = async (req, res) => {
 const deleteEmployee = async (req, res) => {
   try {
     //Write a code here for Deleting all the employees whose salary is greater than 10000
+    const result = await Employee.deleteMany({ salary: { $gt: 10000 } });
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: "No employees found" });
+    }
+    res.status(200).json({ message: "Employees deleted successfully" });
   } catch (err) {
     res.status(500).json({ error: 'Failed to delete employees' });
   }
