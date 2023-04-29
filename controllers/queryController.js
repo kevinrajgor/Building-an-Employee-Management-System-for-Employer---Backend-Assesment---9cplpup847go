d2be97c84f4cec20b09b42379c14622a8a116b81
Kevin Rajgor <kevinrajg95@gmail.com>
2023-04-29 20:05:00 +0530

    Updated

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -7,6 +7,36 @@ const filterQueries = async (req, res) => {
     //2) For Pagination set limit 5 as a default limit and default page is 1
     // Formulae to implementing pagination: (page - 1) * limit
     // For Sorting use    .sort('salary')
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < (await Employee.countDocuments().exec())) {
+      results.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+
+    const sortField = req.query.sortField || "lastName";
+    const sortOrder = parseInt(req.query.sortOrder) || 1;
+
+    results.results = await Employee.find()
+      .sort({ [sortField]: sortOrder })
+      .limit(limit)
+      .skip(startIndex)
+      .exec();
+    res.status(200).json(results);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
